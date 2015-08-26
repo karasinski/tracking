2000d783fa163b5072e50ad5028f322d71267693
John Karasinski <karasinski@gmail.com>
2015-08-26 14:06:30 -0700

    update trials for new experiment

--- trials.py
+++ trials.py
@@ -52,119 +52,24 @@ def generate_path(trial_number, length=240, fps=60):
     return sines
 
 
-ks = [# Training
-      {'trial': 1,
-       'rand_id': 1,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 2,
-       'rand_id': 2,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 3,
-       'rand_id': 3,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 4,
-       'rand_id': 4,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 5,
-       'rand_id': 5,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 6,
-       'rand_id': 1,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 7,
-       'rand_id': 2,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 8,
-       'rand_id': 3,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 9,
-       'rand_id': 4,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 10,
-       'rand_id': 5,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-
-      # Experiment
-      {'trial': 11,  # 28
-       'rand_id': 10,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 12,  # 14
-       'rand_id': 11,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 13,  # 19
-       'rand_id': 10,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 14,  # 15
-       'rand_id': 11,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 15,  # 24
-       'rand_id': 12,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 16,  # 20
-       'rand_id': 13,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 17,  # 11
-       'rand_id': 12,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 18,  # 12
-       'rand_id': 14,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 19,  # 23
-       'rand_id': 13,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 20,  # 22
-       'rand_id': 15,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 21,  # 25
-       'rand_id': 14,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 22,  # 13
-       'rand_id': 15,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 23,  # 18
-       'rand_id': 16,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 24,  # 27
-       'rand_id': 16,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 25,  # 26
-       'rand_id': 17,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False},
-      {'trial': 26,  # 17
-       'rand_id': 17,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 27,  # 21
-       'rand_id': 18,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': True},
-      {'trial': 28,  # 16
-       'rand_id': 18,
-       'feedback': FEEDBACK_ON,
-       'secondary_task': False}
-      ]
+ks = [{'trial': 1},
+      {'trial': 2},
+      {'trial': 3},
+      {'trial': 4},
+      {'trial': 5},
+      {'trial': 6},
+      {'trial': 7},
+      {'trial': 8},
+      {'trial': 9},
+      {'trial': 10},
+      {'wait': 5*60},
+      {'trial': 11},
+      {'trial': 12},
+      {'trial': 13},
+      {'trial': 14},
+      {'trial': 15},
+      {'trial': 16},
+      {'trial': 17},
+      {'trial': 18},
+      {'trial': 19},
+      {'trial': 20}]
